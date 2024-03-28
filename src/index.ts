6d2344f199ad71a070343e92f24d03ec93623e28
hossein <hossein.bajan@gmail.com>
2024-03-28 16:30:50 +0330

    Add express and create server class

--- src/index.ts
+++ src/index.ts
@@ -1,7 +1,38 @@
 import { config } from 'dotenv-safe';
-import { add } from '@src/math/add';
+import express, { Request, Response } from 'express';
 
-config();
+class Server {
+  private app: express.Application;
+  private port: string | number;
 
-console.log('hbsoft');
-console.log(add(1, 2));
+  constructor(port: string | number) {
+    config();
+    this.app = express();
+    this.port = port;
+
+    this.initializeMiddlewares();
+    this.initializeRoutes();
+  }
+
+  private initializeMiddlewares(): void {
+    this.app.use(express.json());
+    this.app.use(express.urlencoded({ extended: true }));
+  }
+
+  private initializeRoutes(): void {
+    this.app.get('/', (req: Request, res: Response) => {
+      res.send('Hello, World!');
+    });
+  }
+
+  public listen(): void {
+    this.app.listen(this.port, () => {
+      console.log(`Server is running on port ${this.port}`);
+    });
+  }
+}
+
+const PORT = process.env.APP_PORT ?? 5000;
+
+const server = new Server(PORT);
+server.listen();
